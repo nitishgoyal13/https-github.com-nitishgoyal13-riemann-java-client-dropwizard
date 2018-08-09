d0a206647ebbe2cf483e5aa5a7eb108acc27e4e1
nitish.goyal <nitish.goyal@phonepe.com>
2018-08-09 15:23:54 +0530

    Removed protobuf dependancy

--- project.clj
+++
@@ -1,5 +0,0 @@
-(defproject riemann.java.client-dopwizard "1.0"
-  :description "Dropwizard integration with riemann java client"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
-  :deploy-repositories [[:sign-releases false :url "https://clojars.org"]]
-)
